613fb1813c5e9802a56bb610b186380ea056d5a5
Yi Peng <ypeng.ollie@gmail.com>
2021-04-02 02:31:48 -0500

    update

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -1,6 +1,9 @@
+import java.nio.file.DirectoryStream.Filter;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class ComputerPlayer extends Player implements IPlayer{
 
@@ -32,11 +35,33 @@ public class ComputerPlayer extends Player implements IPlayer{
 
     @Override
     public Guess getGuess() {
-        Random rand = new Random();
-        return new Guess(this.getAllSuspects().get(rand.nextInt(this.getAllSuspects().size())),
-        this.getAllWeapons().get(rand.nextInt(this.getAllWeapons().size())),
-        this.getAllLocations().get(rand.nextInt(this.getAllLocations().size())),
-        false);
+        Guess g;
+        List<Card> unkownSuspects = this.getAllSuspects().stream().filter(sus -> !this.getOwnedCards().contains(sus)).collect(Collectors.toList());
+        List<Card> unkownWeapons = this.getAllSuspects().stream().filter(w -> !this.getOwnedCards().contains(w)).collect(Collectors.toList());
+        List<Card> unkownLocations = this.getAllSuspects().stream().filter(l -> !this.getOwnedCards().contains(l)).collect(Collectors.toList());
+        int deckCount = this.getAllLocations().size() + this.getAllWeapons().size() + getAllSuspects().size();
+
+        if(deckCount - this.getOwnedCards().size() == 3){ //player has enough information to make a valid accusation
+            g = new Guess(unkownSuspects.get(0), unkownWeapons.get(0), unkownLocations.get(0), true);
+
+        }else if(deckCount - this.getOwnedCards().size() > 3){ 
+            g = new Guess(unkownSuspects.get(0), unkownWeapons.get(0), unkownLocations.get(0), false);
+            
+        }else if(){
+            
+        }
+
+        return g;
+        // Random rand = new Random();
+        // return new Guess(this.getAllSuspects().get(rand.nextInt(this.getAllSuspects().size())),
+        // this.getAllWeapons().get(rand.nextInt(this.getAllWeapons().size())),
+        // this.getAllLocations().get(rand.nextInt(this.getAllLocations().size())),
+        // false);
+    }
+
+    @Override
+    public void receiveInfo(IPlayer ip, Card c){
+        
     }
 
 }
